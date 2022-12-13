fd6231c8c6d168d6f30019cf8bc2e5a3d8b14df1
geumybae <geumybae@gmail.com>
2022-12-13 22:36:20 +0900

    계산기시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+const getNum = (e) => {
+  console.log("e : " , e);
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button>AC</button>
+        <button>+/-</button>
+        <button>%</button>
+        <button>÷</button>  
+      </div>
+      <div>
+        <button value="7" onClick={getNum}>7</button>
+        <button value="8" onClick={getNum}>8</button>
+        <button value="9" onClick={getNum}>9</button>
+        <button>×</button>
+      </div>
+      <div>
+        <button value="4" onClick={getNum}>4</button>
+        <button value="5" onClick={getNum}>5</button>
+        <button value="6" onClick={getNum}>6</button>
+        <button>-</button>
+      </div>
+      <div>
+        <button value="1" onClick={getNum}>1</button>
+        <button value="2" onClick={getNum}>2</button>
+        <button value="3" onClick={getNum}>3</button>
+        <button>+</button>
+      </div>
+      <div>
+        <button value="0" onClick={getNum}>0</button>
+        <button>.</button>
+        <button>=</button>
+      </div>
     </div>
   );
 }
